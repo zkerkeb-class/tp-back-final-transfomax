a3be59f552ed604c7bc939acd72518a03c42108c
transfomax <transfomax02@gmail.com>
2026-02-07 13:15:37 +0100

    added full-art images and python script

--- python-script/dl_art.py
+++ python-script/dl_art.py
@@ -1,7 +1,26 @@
 import openpyxl
 import os
 import re
+import requests
 
+def dl_image(url, number):
+
+    path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    path = os.path.join(path, "assets", "pokemons", "full-art")
+
+    try:
+        response = requests.get(url, stream=True)
+        
+        if response.status_code == 200:
+            with open(os.path.join(path, f"{number}.png"), 'wb') as f:
+                for chunk in response.iter_content(1024):
+                    f.write(chunk)
+            print(f"Succès : Image enregistrée sous {path}")
+        else:
+            print(f"Erreur : Impossible de télécharger (Code {response.status_code})")
+            
+    except Exception as e:
+        print(f"Erreur lors du téléchargement : {e}")
 
 
 dossier_script = os.path.dirname(os.path.abspath(__file__))
@@ -13,7 +32,6 @@ try:
     wb = openpyxl.load_workbook(fichier_excel)
     ws = wb.active 
 
-    data = {}
 
     for row in ws.iter_rows(min_row=2):
         id, image = ws.cell(row=row[0].row, column=3).value, ws.cell(row=row[0].row, column=13).value
@@ -22,16 +40,13 @@ try:
             image = re.search(r'image\("(.+?)"\)', image)
 
             if image:
-                print(image.group(1))
-                data[id] = image.group(1)
+                dl_image(image.group(1), id)
 
             else :
                 continue
 
         else :
             continue
-
-    print(data)    
 
 except Exception as e:
     print(f"Erreur : {e}")
